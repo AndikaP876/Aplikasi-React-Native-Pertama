22b38e4c6a9564f83495fe44e7e00e567d6e7861
AndikaP876 <andikaprasetya876@gmail.com>
2023-12-29 21:00:38 +0700

    implementasi CRUD (create - method POST)

--- src/pages/LocalAPI/index.tsx
+++ src/pages/LocalAPI/index.tsx
@@ -1,31 +1,64 @@
 import {Button, Image, StyleSheet, Text, TextInput, View} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
 
-export default function LocalAPI() {
-  const Item = () => {
-    return (
-      <View style={styles.itemContainer}>
-        <Image
-          source={{uri: 'https://i.pravatar.cc/100?u=andika@pravatar.com'}}
-          style={styles.avatar}
-        />
-        <View style={styles.desc}>
-          <Text style={styles.descName}>Nama Lengkap</Text>
-          <Text style={styles.descEmail}>Email</Text>
-          <Text style={styles.descBidang}>Bidang</Text>
-        </View>
-        <Text style={styles.delete}>X</Text>
+const Item = () => {
+  return (
+    <View style={styles.itemContainer}>
+      <Image
+        source={{uri: 'https://i.pravatar.cc/100?u=andika@pravatar.com'}}
+        style={styles.avatar}
+      />
+      <View style={styles.desc}>
+        <Text style={styles.descName}>Nama Lengkap</Text>
+        <Text style={styles.descEmail}>Email</Text>
+        <Text style={styles.descBidang}>Bidang</Text>
       </View>
-    );
+      <Text style={styles.delete}>X</Text>
+    </View>
+  );
+};
+export default function LocalAPI() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [bidang, setBidang] = useState('');
+
+  const submit = () => {
+    const data = {
+      name,
+      email,
+      bidang,
+    };
+    axios.post('http://10.0.2.2:3000/users', data).then(res => {
+      console.log('res: ', res);
+      setName('');
+      setEmail('');
+      setBidang('');
+    });
   };
   return (
     <View style={styles.container}>
       <Text style={styles.textTitle}>Local API (JSON Server)</Text>
       <Text>Masukan Anggota Kabayan Coding</Text>
-      <TextInput placeholder="Nama Lengkap" style={styles.input} />
-      <TextInput placeholder="Email" style={styles.input} />
-      <TextInput placeholder="Bidang" style={styles.input} />
-      <Button title="Simpan" />
+      <TextInput
+        placeholder="Nama Lengkap"
+        style={styles.input}
+        value={name}
+        onChangeText={value => setName(value)}
+      />
+      <TextInput
+        placeholder="Email"
+        style={styles.input}
+        value={email}
+        onChangeText={value => setEmail(value)}
+      />
+      <TextInput
+        placeholder="Bidang"
+        style={styles.input}
+        value={bidang}
+        onChangeText={value => setBidang(value)}
+      />
+      <Button title="Simpan" onPress={submit} />
       <View style={styles.line} />
       <Item />
       <Item />
@@ -50,5 +83,5 @@ const styles = StyleSheet.create({
   descName: {fontSize: 20, fontWeight: 'bold'},
   descEmail: {fontSize: 16},
   descBidang: {fontSize: 12, marginTop: 8},
-  delete: {fontSize: 20, fontWeight: 'bold', color: 'red'}
+  delete: {fontSize: 20, fontWeight: 'bold', color: 'red'},
 });
